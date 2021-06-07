7fe3bd934fdfa8ffac7c1de0122a106ae03aa617
Andrew <andrew.fleer@gmail.com>
2021-06-06 20:21:00 -0500

    Clean up imports

--- src/test/java/com/andrewfleer/movies/MoviesApplicationTests.java
+++ src/test/java/com/andrewfleer/movies/MoviesApplicationTests.java
@@ -1,15 +1,10 @@
 package com.andrewfleer.movies;
 
 import com.andrewfleer.movies.dto.MovieDTO;
-import com.andrewfleer.movies.entity.Movie;
-import com.andrewfleer.movies.repository.MovieRepository;
 import com.andrewfleer.movies.service.MovieService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
