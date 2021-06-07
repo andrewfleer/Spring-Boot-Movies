891a7bfb6e085c716a3cdf1b3e2165c34600258a
Andrew <andrew.fleer@gmail.com>
2021-06-06 20:10:46 -0500

    Added unit tests

--- src/test/java/com/andrewfleer/movies/MoviesApplicationTests.java
+++ src/test/java/com/andrewfleer/movies/MoviesApplicationTests.java
@@ -1,13 +1,65 @@
 package com.andrewfleer.movies;
 
+import com.andrewfleer.movies.dto.MovieDTO;
+import com.andrewfleer.movies.entity.Movie;
+import com.andrewfleer.movies.repository.MovieRepository;
+import com.andrewfleer.movies.service.MovieService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
 
 @SpringBootTest
 class MoviesApplicationTests {
 
+    @Autowired
+    private MovieService movieService;
+
     @Test
-    void contextLoads() {
+    public void should_find_movies() {
+        List<MovieDTO> movies = movieService.listAllMovies(0);
+
+        int zero_movies = 0;
+        assertThat(movies).hasSizeGreaterThan(zero_movies);
     }
 
+    @Test
+    public void should_find_movies_in_2003() {
+        List<MovieDTO> movies = movieService.getMoviesByYear(2003, 0);
+
+        int zero_movies = 0;
+        assertThat(movies).hasSizeGreaterThan(zero_movies);
+    }
+
+    @Test
+    public void should_find_no_movies_in_1000() {
+        List<MovieDTO> movies = movieService.getMoviesByYear(1000, 0);
+
+        int zero_movies = 0;
+        assertThat(movies).hasSize(zero_movies);
+    }
+
+    @Test
+    public void should_find_comedy_movies() {
+        List<MovieDTO> movies = movieService.getMoviesByGenre("Comedy", 0);
+
+        int zero_movies = 0;
+        assertThat(movies).hasSizeGreaterThan(zero_movies);
+    }
+
+    @Test
+    public void should_find_no_foobar_movies() {
+        List<MovieDTO> movies = movieService.getMoviesByGenre("foobar", 0);
+
+        int zero_movies = 0;
+        assertThat(movies).hasSize(zero_movies);
+    }
 }
